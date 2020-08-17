7b1cdf95b135f9340499d6ca2e40ab515ae138bc
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-17 21:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function wrapAdjective(symbol="*") {
      return `You are ${symbol}${adj}${symbol}!`;
    }
  }
+ let Calculator = {
+   add: function (x, y) {
+     return x + y;
+   },
+   subtract: function (x, y) {
+     return x - y;
+   },
+   multiply: function (x, y) {
+     return x * y;
+   },
+   divide: function (x, y) {
+     return x / y;
+   },
+ };
