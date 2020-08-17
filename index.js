2cec96946e62b31c877a9abeb4946cedb2873a56
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-17 20:11:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 function saturdayFun(activity="roller-skate"){
   return `This Saturday, I want to ${activity}!`;
 }
-const mondayWork=function(activity="go to the office"){
-  return `This Monday, I will ${activity}`;
+// const mondayWork=function(activity="go to the office"){
+//   return `This Monday, I will ${activity}`;
+// }
+function mondayWork(act="go to the office") {
+    return `This Monday, I will ${act}.`;
 }
